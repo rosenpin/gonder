b29647edd1eff6dfcec8a15a5baafaf4bcefbf2c
Tomer Rosenfeld <tomer.rosenfeld@mixtiles.com>
2025-09-25 09:32:48 +0800

    added chatview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'package:chatview/chatview.dart';
 
 void main() {
   runApp(const MainApp());
@@ -39,7 +40,7 @@ class _TinderHomeState extends State<TinderHome> {
       body: SafeArea(
         child: IndexedStack(
           index: _selectedIndex,
-          children: const [SwipeDeckView(), ComingSoonView()],
+          children: const [SwipeDeckView(), ChatViewPage()],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -144,39 +145,40 @@ class _SwipeDeckViewState extends State<SwipeDeckView> {
                           cardsCount: _profiles.length,
                           isLoop: false,
                           padding: EdgeInsets.zero,
-                          numberOfCardsDisplayed:
-                              _profiles.length > 1 ? 2 : _profiles.length,
+                          numberOfCardsDisplayed: _profiles.length > 1
+                              ? 2
+                              : _profiles.length,
                           allowedSwipeDirection:
                               const AllowedSwipeDirection.only(
-                            left: true,
-                            right: true,
-                            up: true,
-                          ),
+                                left: true,
+                                right: true,
+                                up: true,
+                              ),
                           onSwipe: _handleSwipe,
-                          onUndo: (
-                            int? previousIndex,
-                            int currentIndex,
-                            CardSwiperDirection direction,
-                          ) {
-                            setState(() {
-                              _isDeckFinished = false;
-                            });
-                            return true;
-                          },
-                          onEnd: () =>
-                              setState(() => _isDeckFinished = true),
-                          cardBuilder: (
-                            context,
-                            index,
-                            percentThresholdX,
-                            percentThresholdY,
-                          ) {
-                            final profile = _profiles[index];
-                            return _ProfileCard(profile: profile);
-                          },
+                          onUndo:
+                              (
+                                int? previousIndex,
+                                int currentIndex,
+                                CardSwiperDirection direction,
+                              ) {
+                                setState(() {
+                                  _isDeckFinished = false;
+                                });
+                                return true;
+                              },
+                          onEnd: () => setState(() => _isDeckFinished = true),
+                          cardBuilder:
+                              (
+                                context,
+                                index,
+                                percentThresholdX,
+                                percentThresholdY,
+                              ) {
+                                final profile = _profiles[index];
+                                return _ProfileCard(profile: profile);
+                              },
                         ),
-                        if (_isDeckFinished)
-                          const _EmptyDeckPlaceholder(),
+                        if (_isDeckFinished) const _EmptyDeckPlaceholder(),
                       ],
                     ),
                   ),
@@ -383,17 +385,209 @@ class _EmptyDeckPlaceholder extends StatelessWidget {
   }
 }
 
-class ComingSoonView extends StatelessWidget {
-  const ComingSoonView({super.key});
+class ChatViewPage extends StatefulWidget {
+  const ChatViewPage({super.key});
+
+  @override
+  State<ChatViewPage> createState() => _ChatViewPageState();
+}
+
+class _ChatViewPageState extends State<ChatViewPage> {
+  late ChatController chatController;
+  final ChatUser currentUser = ChatUser(id: '1', name: 'You');
+  final ChatUser otherUser = ChatUser(id: '2', name: 'Bella');
+
+  List<Message> messageList = [
+    Message(
+      id: '1',
+      message: 'Hey! How are you doing? 🐐',
+      createdAt: DateTime.now().subtract(const Duration(minutes: 5)),
+      sentBy: '2',
+      messageType: MessageType.text,
+    ),
+    Message(
+      id: '2',
+      message: 'I\'m doing great! Just finished grazing in the meadow 🌱',
+      createdAt: DateTime.now().subtract(const Duration(minutes: 3)),
+      sentBy: '1',
+      messageType: MessageType.text,
+    ),
+    Message(
+      id: '3',
+      message:
+          'That sounds amazing! Would you like to meet up at the barn later? ❤️',
+      createdAt: DateTime.now().subtract(const Duration(minutes: 1)),
+      sentBy: '2',
+      messageType: MessageType.text,
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    chatController = ChatController(
+      initialMessageList: messageList,
+      scrollController: ScrollController(),
+      currentUser: currentUser,
+      otherUsers: [otherUser],
+    );
+  }
+
+  void onSendTap(
+    String message,
+    ReplyMessage replyMessage,
+    MessageType messageType,
+  ) {
+    final newMessage = Message(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      message: message,
+      createdAt: DateTime.now(),
+      sentBy: currentUser.id,
+      replyMessage: replyMessage,
+      messageType: messageType,
+    );
+    chatController.addMessage(newMessage);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Chat tab coming soon',
-        style: Theme.of(context).textTheme.titleLarge,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat with Bella 💕'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+      ),
+      body: ChatView(
+        chatController: chatController,
+        onSendTap: onSendTap,
+        chatViewState: ChatViewState.hasMessages,
+        chatViewStateConfig: const ChatViewStateConfiguration(
+          loadingWidgetConfig: ChatViewStateWidgetConfiguration(
+            loadingIndicatorColor: Colors.pink,
+          ),
+          onReloadButtonTap: null,
+        ),
+        typeIndicatorConfig: TypeIndicatorConfiguration(
+          flashingCircleBrightColor: Theme.of(context).colorScheme.primary,
+          flashingCircleDarkColor: Colors.grey,
+        ),
+        appBar: ChatViewAppBar(
+          elevation: 2,
+          backGroundColor: Theme.of(context).colorScheme.primary,
+          profilePicture: 'assets/2.png',
+          chatTitle: 'Bella',
+          chatTitleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+          userStatus: 'Online',
+          userStatusTextStyle: const TextStyle(color: Colors.white70),
+        ),
+        chatBubbleConfig: ChatBubbleConfiguration(
+          outgoingChatBubbleConfig: ChatBubble(
+            linkPreviewConfig: const LinkPreviewConfiguration(
+              backgroundColor: Colors.white,
+              bodyStyle: TextStyle(color: Colors.grey, fontSize: 16),
+              titleStyle: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+            receiptsWidgetConfig: const ReceiptsWidgetConfig(
+              showReceiptsIn: ShowReceiptsIn.all,
+            ),
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          inComingChatBubbleConfig: ChatBubble(
+            linkPreviewConfig: const LinkPreviewConfiguration(
+              linkStyle: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.underline,
+              ),
+              backgroundColor: Colors.grey,
+              bodyStyle: TextStyle(color: Colors.white, fontSize: 16),
+              titleStyle: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            textStyle: const TextStyle(color: Colors.white),
+            onMessageRead: (message) {},
+            senderNameTextStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+            ),
+            color: Colors.grey.shade600,
+          ),
+        ),
+        sendMessageConfig: SendMessageConfiguration(
+          imagePickerIconsConfig: ImagePickerIconsConfiguration(
+            cameraIconColor: Theme.of(context).colorScheme.primary,
+            galleryIconColor: Theme.of(context).colorScheme.primary,
+          ),
+          replyMessageColor: Colors.grey,
+          defaultSendButtonColor: Theme.of(context).colorScheme.primary,
+          replyDialogColor: Colors.grey.shade100,
+          replyTitleColor: Theme.of(context).colorScheme.primary,
+          textFieldBackgroundColor: Colors.white,
+          closeIconColor: Theme.of(context).colorScheme.primary,
+          textFieldConfig: const TextFieldConfiguration(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+            maxLines: 5,
+            minLines: 1,
+            textStyle: TextStyle(color: Colors.black),
+            hintText: 'Type a message...',
+            hintStyle: TextStyle(color: Colors.grey),
+          ),
+          micIconColor: Theme.of(context).colorScheme.primary,
+          voiceRecordingConfiguration: VoiceRecordingConfiguration(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            recorderIconColor: Colors.white,
+            waveStyle: const WaveStyle(
+              showMiddleLine: false,
+              waveColor: Colors.white,
+              extendWaveform: true,
+            ),
+          ),
+        ),
+        chatBackgroundConfig: ChatBackgroundConfiguration(
+          messageTimeIconColor: Colors.grey,
+          defaultGroupSeparatorConfig: DefaultGroupSeparatorConfiguration(
+            textStyle: TextStyle(color: Colors.grey.shade600, fontSize: 17),
+          ),
+          backgroundColor: Colors.grey.shade50,
+        ),
+        profileCircleConfig: const ProfileCircleConfiguration(
+          profileImageUrl: 'assets/2.png',
+        ),
+        repliedMessageConfig: RepliedMessageConfiguration(
+          backgroundColor: Theme.of(
+            context,
+          ).colorScheme.primary.withOpacity(0.2),
+          verticalBarColor: Theme.of(context).colorScheme.primary,
+          repliedMsgAutoScrollConfig: RepliedMsgAutoScrollConfig(
+            enableHighlightRepliedMsg: true,
+            highlightColor: Theme.of(
+              context,
+            ).colorScheme.primary.withOpacity(0.3),
+            highlightScale: 1.1,
+          ),
+          textStyle: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0.25,
+          ),
+          replyTitleTextStyle: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        swipeToReplyConfig: SwipeToReplyConfiguration(
+          replyIconColor: Theme.of(context).colorScheme.primary,
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    chatController.dispose();
+    super.dispose();
   }
 }
 
